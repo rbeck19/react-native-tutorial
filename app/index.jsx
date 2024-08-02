246850a61b80b1ac8b4094f39185abfc90959392
rbeck19 <ryan.beck219@gmail.com>
2024-08-02 17:53:54 -0400

    Sign in page

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,16 @@
-import {SafeAreaView, Text, View, ScrollView, Image } from 'react-native'
+import { Text, View, ScrollView, Image } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router'
+import { Redirect, router } from 'expo-router'
 import {images} from '../constants'
+import CustomButton from '../components/CustomButton'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
  
 const index = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full h-full items-center justify-center px-4">
+        <View className="w-full min-h-[85vh] items-center justify-center px-4">
           <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode='contain'/>
           <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode='contain'/>
           <View className="relative mt-5">
@@ -15,10 +18,15 @@ const index = () => {
               Discover Endless Possibilities with{' '}
               <Text className="text-secondary-200">Aora</Text>
             </Text>
-
+            <Image source={images.path} className="w-[136px] h-[15px] absolute -bottom-2 -right-8" resizeMode='contain'/>
           </View>
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+              Where creativity meets innovation: embark on a journey of limitless exploration with Aora
+            </Text>
+            <CustomButton title="Continue with Email" handlePress={()=>router.push('/sign-in')} containerStyles="w-full mt-7"/>
         </View>
       </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light' />
     </SafeAreaView>
   )
 }
