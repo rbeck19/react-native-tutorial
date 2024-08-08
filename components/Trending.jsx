281d749953017fd82d1811a6b1ca8e0cbce3d8fc
rbeck19 <ryan.beck219@gmail.com>
2024-08-08 11:40:58 -0400

    horizontal list animation

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -9,14 +9,14 @@ const zoomIn = {
     scale: 0.9
   },
   1: {
-    scale:1
+    scale:1.1
   }
 }
 const zoomOut = {
-  1: {
-    scale: 1
-  },
   0: {
+    scale: 1.1
+  },
+  1: {
     scale: 0.9
   }
 }
@@ -26,7 +26,7 @@ const TrendingItem = ({activeItem, item}) => {
   return (
     <Animatable.View
       className="mr-5"
-      animation={activeItem.$id === item.$id ? zoomIn : zoomOut}
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
       duration={500}
     >
       {play ? 
@@ -48,7 +48,14 @@ const TrendingItem = ({activeItem, item}) => {
 }
 
 const Trending = ({posts}) => {
-  const [activeItem, setActiveItem] = useState(posts[0])
+  const [activeItem, setActiveItem] = useState(posts[1])
+
+  const viewableItemsChanged = ({viewableItems}) => {
+    if(viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key)
+    }
+  }
+
   return (
     <FlatList
         data={posts}
@@ -57,6 +64,9 @@ const Trending = ({posts}) => {
            <TrendingItem activeItem={activeItem} item={item}/>
         )}
         horizontal
+        onViewableItemsChanged={viewableItemsChanged}
+        viewabilityConfig={{itemVisiblePercentThreshold: 70}}
+        contentOffset={{x:170}}
     />
   )
 }
