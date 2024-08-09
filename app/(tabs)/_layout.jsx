26b878589a86b02c841c65d32c84f816db7293d3
rbeck19 <ryan.beck219@gmail.com>
2024-08-09 14:38:30 -0400

    remove unused tab

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -41,14 +41,14 @@ tabBarStyle: {
     focused={focused}
     />
   ) }}/>
-    <Tabs.Screen name="bookmark" options={{title:'Bookmark', headerShown: false, tabBarIcon:({color, focused}) =>(
-    <TabIcon 
-    icon={icons.bookmark}
-    color={color}
-    name="Bookmark"
-    focused={focused}
-    />
-  ) }}/>
+    {/* <Tabs.Screen name="bookmark" options={{title:'Bookmark', headerShown: false, tabBarIcon:({color, focused}) =>(
+      <TabIcon 
+      icon={icons.bookmark}
+      color={color}
+      name="Bookmark"
+      focused={focused}
+      />
+  ) }}/> */}
     <Tabs.Screen name="create" options={{title:'Create', headerShown: false, tabBarIcon:({color, focused}) =>(
     <TabIcon 
     icon={icons.plus}
